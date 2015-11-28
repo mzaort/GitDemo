96b9cf271b014fbd095831a255af9bde854be5d7
mzaort <mzaort@gmail.com>
2015-11-28 23:27:15 +0800

    Add one function

--- src/mzaort/java8/Lambada.java
+++ src/mzaort/java8/Lambada.java
@@ -1,7 +1,9 @@
 package mzaort.java8;
 
+import java.util.Arrays;
+
 public class Lambada {
 	public static void main(String[] args) {
-		
+		Arrays.asList(1, 2, 3).stream().filter(x -> x % 2 == 0).forEach(System.out::println);
 	}
 }
