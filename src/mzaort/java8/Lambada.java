9aa3411f9ddfc9d68d89f9864490a8e0a83b0f9f
mzaort <mzaort@gmail.com>
2015-11-28 23:43:58 +0800

    Okay

--- src/mzaort/java8/Lambada.java
+++ src/mzaort/java8/Lambada.java
@@ -3,7 +3,9 @@ package mzaort.java8;
 import java.util.Arrays;
 
 public class Lambada {
+	public static final String STD = "stdlib.c";
 	public static void main(String[] args) {
 		Arrays.asList(1, 2, 3).stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+		System.out.println(STD);
 	}
 }
